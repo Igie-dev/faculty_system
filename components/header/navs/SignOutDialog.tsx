484f9e843472d214b7af6c9a08a64d048774545a
Igie.dev <baldesansoigieboysanchez@gmail.com>
2024-05-10 22:21:17 +0800

    Signout dialog

---
+++ components/header/navs/SignOutDialog.tsx
@@ -0,0 +1,44 @@
+"use client";
+import React from "react";
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+  AlertDialogTrigger,
+} from "@/components/ui/alert-dialog";
+import { signOut } from "next-auth/react";
+export default function SignOutDialog({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <AlertDialog>
+      <AlertDialogTrigger asChild>{children}</AlertDialogTrigger>
+      <AlertDialogContent>
+        <AlertDialogHeader>
+          <AlertDialogTitle className="flex flex-col">
+            <h1>Signing out</h1>
+            <h5>Are you absolutely sure?</h5>
+          </AlertDialogTitle>
+          <AlertDialogDescription>
+            This action will log you out of your account, terminating your
+            current session. Any unsaved data will be lost. You will need to
+            re-enter your credentials to access your account again.
+          </AlertDialogDescription>
+        </AlertDialogHeader>
+        <AlertDialogFooter>
+          <AlertDialogCancel>Cancel</AlertDialogCancel>
+          <AlertDialogAction onClick={() => signOut()}>
+            Continue
+          </AlertDialogAction>
+        </AlertDialogFooter>
+      </AlertDialogContent>
+    </AlertDialog>
+  );
+}
