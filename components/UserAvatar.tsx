2c3a6ead8ff98582b2200df63764d2cf3bf06a95
Igie.dev <baldesansoigieboysanchez@gmail.com>
2024-05-13 16:56:20 +0800

    add placeholder

--- components/UserAvatar.tsx
+++ components/UserAvatar.tsx
@@ -2,20 +2,24 @@
 import React from "react";
 import { Avatar, AvatarFallback, AvatarImage } from "./ui/avatar";
 import { useSession } from "next-auth/react";
-import { Skeleton } from "./ui/skeleton";
 export default function UserAvatar() {
   const { data: session } = useSession();
-  //TODO
-  //if no data no session image
-  //Query user avatar by id
+  const link = session?.user?.image as string;
+
   return (
     <Avatar className="w-full h-full">
       <AvatarImage
-        src={session?.user?.image as string}
+        src={link ?? "/placeholder.jpg"}
         className="object-cover w-full h-full"
       />
       <AvatarFallback>
-        <Skeleton />
+        <AvatarFallback>
+          <AvatarImage
+            src="/placeholder.jpg"
+            loading="lazy"
+            className="object-cover w-full h-full "
+          />
+        </AvatarFallback>
       </AvatarFallback>
     </Avatar>
   );
