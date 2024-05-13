5277d11f93dda4370101f6ab91ebef854d80565c
Igie.dev <baldesansoigieboysanchez@gmail.com>
2024-05-13 16:56:07 +0800

    use nav component

--- components/header/navs/DesktopNav.tsx
+++ components/header/navs/DesktopNav.tsx
@@ -1,28 +1,12 @@
 "use client";
 import React, { useState } from "react";
 import { Button } from "../../ui/button";
-import {
-  ArrowLeft,
-  Bell,
-  CalendarClock,
-  Download,
-  FileText,
-  Grid2X2,
-  Home,
-  LogOut,
-  Megaphone,
-  Menu,
-  User,
-  Users,
-} from "lucide-react";
-import { usePathname } from "next/navigation";
-import Link from "next/link";
-import SignOutDialog from "./SignOutDialog";
+import { ArrowLeft, Menu } from "lucide-react";
+import Nav from "./Nav";
 export default function DesktopNav() {
-  const pathname = usePathname();
   const [isExpanded, setIsExpanded] = useState(false);
   return (
-    <nav
+    <div
       className={`hidden lg:flex  p-2 flex-col ${
         isExpanded ? "w-[16rem]" : "w-[5rem]"
       }`}
@@ -35,6 +19,7 @@ export default function DesktopNav() {
         <Button
           variant="secondary"
           size="icon"
+          className="text-muted-foreground"
           onClick={() => setIsExpanded((prev) => !prev)}
         >
           {!isExpanded ? (
@@ -44,197 +29,7 @@ export default function DesktopNav() {
           )}
         </Button>
       </div>
-      <ul className="w-full flex flex-col gap-1 pt-8">
-        <Button
-          variant="ghost"
-          size="lg"
-          asChild
-          className={`gap-4 flex items-center w-full text-sm rounded-lg  ${
-            isExpanded ? "px-4 justify-start" : "justify-center px-0"
-          } ${
-            pathname === "/dashboard"
-              ? "bg-primary text-background hover:bg-primary hover:text-background transition-none"
-              : "bg-background"
-          }`}
-        >
-          <Link href="/dashboard">
-            <Home absoluteStrokeWidth size={22} />
-            <span className={` ${isExpanded ? "flex" : "hidden"}`}>
-              Dashboard
-            </span>
-          </Link>
-        </Button>
-        <Button
-          variant="ghost"
-          size="lg"
-          asChild
-          className={`gap-4 flex items-center w-full text-sm rounded-lg  ${
-            isExpanded ? "px-4 justify-start" : "justify-center px-0"
-          } ${
-            pathname === "/faculties"
-              ? "bg-primary text-background hover:bg-primary hover:text-background transition-none"
-              : "bg-background"
-          }`}
-        >
-          <Link href="/faculties">
-            <Users absoluteStrokeWidth size={22} />
-            <span className={` ${isExpanded ? "flex" : "hidden"}`}>
-              Faculties
-            </span>
-          </Link>
-        </Button>
-        <Button
-          variant="ghost"
-          size="lg"
-          asChild
-          className={`gap-4 flex items-center w-full text-sm rounded-lg  ${
-            isExpanded ? "px-4 justify-start" : "justify-center px-0"
-          } ${
-            pathname === "/departments"
-              ? "bg-primary text-background hover:bg-primary hover:text-background transition-none"
-              : "bg-background"
-          }`}
-        >
-          <Link href="/departments">
-            <Grid2X2 absoluteStrokeWidth size={22} />
-            <span className={` ${isExpanded ? "flex" : "hidden"}`}>
-              Departments
-            </span>
-          </Link>
-        </Button>
-        <Button
-          variant="ghost"
-          size="lg"
-          asChild
-          className={`gap-4 flex items-center w-full text-sm rounded-lg  ${
-            isExpanded ? "px-4 justify-start" : "justify-center px-0"
-          } ${
-            pathname === "/announcements"
-              ? "bg-primary text-background hover:bg-primary hover:text-background transition-none"
-              : "bg-background"
-          }`}
-        >
-          <Link href="/announcements">
-            <Megaphone absoluteStrokeWidth size={22} />
-            <span className={` ${isExpanded ? "flex" : "hidden"}`}>
-              Announcements
-            </span>
-          </Link>
-        </Button>
-        <Button
-          variant="ghost"
-          size="lg"
-          asChild
-          className={`gap-4 flex items-center w-full text-sm rounded-lg  ${
-            isExpanded ? "px-4 justify-start" : "justify-center px-0"
-          } ${
-            pathname === "/submissions"
-              ? "bg-primary text-background hover:bg-primary hover:text-background transition-none"
-              : "bg-background"
-          }`}
-        >
-          <Link href="/submissions">
-            <FileText absoluteStrokeWidth size={22} />
-            <span className={` ${isExpanded ? "flex" : "hidden"}`}>
-              Submissions
-            </span>
-          </Link>
-        </Button>
-        <div className="flex flex-col gap-2 pt-5 mt-5 border-t h-fit">
-          <Button
-            variant="ghost"
-            size="lg"
-            asChild
-            className={`gap-4 flex items-center w-full text-sm rounded-lg  ${
-              isExpanded ? "px-4 justify-start" : "justify-center px-0"
-            } ${
-              pathname === "/profile"
-                ? "bg-primary text-background hover:bg-primary hover:text-background transition-none"
-                : "bg-background"
-            }`}
-          >
-            <Link href="/profile">
-              <User absoluteStrokeWidth size={22} />
-              <span className={` ${isExpanded ? "flex" : "hidden"}`}>
-                Profile
-              </span>
-            </Link>
-          </Button>
-          <Button
-            variant="ghost"
-            size="lg"
-            asChild
-            className={`gap-4 flex items-center w-full text-sm rounded-lg  ${
-              isExpanded ? "px-4 justify-start" : "justify-center px-0"
-            } ${
-              pathname === "/mytask"
-                ? "bg-primary text-background hover:bg-primary hover:text-background transition-none"
-                : "bg-background"
-            }`}
-          >
-            <Link href="/mytask">
-              <CalendarClock absoluteStrokeWidth size={22} />
-              <span className={` ${isExpanded ? "flex" : "hidden"}`}>
-                Mytask
-              </span>
-            </Link>
-          </Button>
-          <Button
-            variant="ghost"
-            size="lg"
-            asChild
-            className={`gap-4 flex items-center w-full text-sm rounded-lg  ${
-              isExpanded ? "px-4 justify-start" : "justify-center px-0"
-            } ${
-              pathname === "/notifications"
-                ? "bg-primary text-background hover:bg-primary hover:text-background transition-none"
-                : "bg-background"
-            }`}
-          >
-            <Link href="/notifications">
-              <Bell absoluteStrokeWidth size={22} />
-              <span className={` ${isExpanded ? "flex" : "hidden"}`}>
-                Notifications
-              </span>
-            </Link>
-          </Button>
-          <Button
-            variant="ghost"
-            size="lg"
-            asChild
-            className={`gap-4 flex items-center w-full text-sm rounded-lg  ${
-              isExpanded ? "px-4 justify-start" : "justify-center px-0"
-            } ${
-              pathname === "/downloadables"
-                ? "bg-primary text-background hover:bg-primary hover:text-background transition-none"
-                : "bg-background"
-            }`}
-          >
-            <Link href="/downloadables">
-              <Download absoluteStrokeWidth size={22} />
-              <span className={` ${isExpanded ? "flex" : "hidden"}`}>
-                Downloadables
-              </span>
-            </Link>
-          </Button>
-        </div>
-        <div className="flex flex-col gap-2 pt-5 mt-10 border-t h-fit">
-          <SignOutDialog>
-            <Button
-              variant="ghost"
-              size="lg"
-              className={`gap-4 flex items-center w-full text-sm rounded-lg  ${
-                isExpanded ? "px-4 justify-start" : "justify-center px-0"
-              }`}
-            >
-              <LogOut absoluteStrokeWidth size={22} />
-              <span className={` ${isExpanded ? "flex" : "hidden"}`}>
-                Signout
-              </span>
-            </Button>
-          </SignOutDialog>
-        </div>
-      </ul>
-    </nav>
+      <Nav isExpanded={isExpanded} />
+    </div>
   );
 }
--- components/header/navs/MobileNav.tsx
+++ components/header/navs/MobileNav.tsx
@@ -1,28 +1,13 @@
 "use client";
 import React, { useState } from "react";
 import { Button } from "../../ui/button";
-import {
-  Bell,
-  CalendarClock,
-  Download,
-  FileText,
-  Grid2X2,
-  Home,
-  LogOut,
-  Megaphone,
-  Menu,
-  User,
-  Users,
-  X,
-} from "lucide-react";
-import Link from "next/link";
-import { usePathname } from "next/navigation";
-import SignOutDialog from "./SignOutDialog";
+import { Menu, X } from "lucide-react";
+
+import Nav from "./Nav";
 export default function MobileNav() {
   const [isShow, setIsShow] = useState(true);
-  const pathname = usePathname();
   return (
-    <nav
+    <div
       className={`lg:hidden w-full h-full absolute top-0 left-0 z-50 transition-all delay-150 bg-background/50 ${
         isShow ? "translate-x-0" : "-translate-x-full"
       }`}
@@ -32,7 +17,7 @@ export default function MobileNav() {
           variant="secondary"
           size="icon"
           onClick={() => setIsShow(true)}
-          className="absolute top-3 -right-12"
+          className="absolute top-3 -right-12 text-muted-foreground "
         >
           <Menu absoluteStrokeWidth size={20} />
         </Button>
@@ -44,169 +29,14 @@ export default function MobileNav() {
               variant="secondary"
               size="icon"
               onClick={() => setIsShow(false)}
-              className="absolute top-2 -right-5"
+              className="absolute top-2 -right-5 text-muted-foreground "
             >
               <X absoluteStrokeWidth size={20} />
             </Button>
           ) : null}
         </div>
-        <ul className="w-full flex flex-col gap-1 px-1  pt-4">
-          <Button
-            variant="ghost"
-            size="lg"
-            asChild
-            onClick={() => setIsShow(false)}
-            className={`rounded-lg justify-start gap-4 pl-4 flex items-center w-[90%] text-sm  transition-all ${
-              pathname === "/dashboard"
-                ? "bg-primary text-background hover:bg-primary hover:text-background"
-                : "bg-background"
-            }`}
-          >
-            <Link href="/dashboard">
-              <Home absoluteStrokeWidth size={22} /> <span>Dashboard</span>
-            </Link>
-          </Button>
-          <Button
-            variant="ghost"
-            size="lg"
-            asChild
-            onClick={() => setIsShow(false)}
-            className={`rounded-lg justify-start gap-4 pl-4 flex items-center w-[90%] text-sm  transition-all ${
-              pathname === "/faculties"
-                ? "bg-primary text-background hover:bg-primary hover:text-background"
-                : "bg-background"
-            }`}
-          >
-            <Link href="/faculties">
-              <Users absoluteStrokeWidth size={22} /> <span>Faculties</span>
-            </Link>
-          </Button>
-          <Button
-            variant="ghost"
-            size="lg"
-            asChild
-            onClick={() => setIsShow(false)}
-            className={`rounded-lg justify-start gap-4 pl-4 flex items-center w-[90%] text-sm  transition-all ${
-              pathname === "/departments"
-                ? "bg-primary text-background hover:bg-primary hover:text-background"
-                : "bg-background"
-            }`}
-          >
-            <Link href="/departments">
-              <Grid2X2 absoluteStrokeWidth size={22} /> <span>Departments</span>
-            </Link>
-          </Button>
-          <Button
-            variant="ghost"
-            size="lg"
-            asChild
-            onClick={() => setIsShow(false)}
-            className={`rounded-lg justify-start gap-4 pl-4 flex items-center w-[90%] text-sm  transition-all ${
-              pathname === "/announcements"
-                ? "bg-primary text-background hover:bg-primary hover:text-background"
-                : "bg-background"
-            }`}
-          >
-            <Link href="/announcements">
-              <Megaphone absoluteStrokeWidth size={22} />{" "}
-              <span>Announcements</span>
-            </Link>
-          </Button>
-          <Button
-            variant="ghost"
-            size="lg"
-            asChild
-            onClick={() => setIsShow(false)}
-            className={`rounded-lg justify-start gap-4 pl-4 flex items-center w-[90%] text-sm  transition-all ${
-              pathname === "/submissions"
-                ? "bg-primary text-background hover:bg-primary hover:text-background"
-                : "bg-background"
-            }`}
-          >
-            <Link href="/submissions">
-              <FileText absoluteStrokeWidth size={22} />{" "}
-              <span>Submissions</span>
-            </Link>
-          </Button>
-          <div className="flex flex-col gap-2 pt-5 mt-5 border-t h-fit">
-            <Button
-              variant="ghost"
-              size="lg"
-              asChild
-              onClick={() => setIsShow(false)}
-              className={`rounded-lg justify-start gap-4 pl-4 flex items-center w-[90%] text-sm  transition-all ${
-                pathname === "/profile"
-                  ? "bg-primary text-background hover:bg-primary hover:text-background"
-                  : "bg-background"
-              }`}
-            >
-              <Link href="/profile">
-                <User absoluteStrokeWidth size={22} /> <span>Profile</span>
-              </Link>
-            </Button>
-            <Button
-              variant="ghost"
-              size="lg"
-              asChild
-              onClick={() => setIsShow(false)}
-              className={`rounded-lg justify-start gap-4 pl-4 flex items-center w-[90%] text-sm  transition-all ${
-                pathname === "/mytask"
-                  ? "bg-primary text-background hover:bg-primary hover:text-background"
-                  : "bg-background"
-              }`}
-            >
-              <Link href="/mytask">
-                <CalendarClock absoluteStrokeWidth size={22} />
-                <span>MyTask</span>
-              </Link>
-            </Button>
-            <Button
-              variant="ghost"
-              size="lg"
-              asChild
-              onClick={() => setIsShow(false)}
-              className={`rounded-lg justify-start gap-4 pl-4 flex items-center w-[90%] text-sm  transition-all ${
-                pathname === "/notifications"
-                  ? "bg-primary text-background hover:bg-primary hover:text-background"
-                  : "bg-background"
-              }`}
-            >
-              <Link href="/notifications">
-                <Bell absoluteStrokeWidth size={22} />
-                <span>Notifications</span>
-              </Link>
-            </Button>
-            <Button
-              variant="ghost"
-              size="lg"
-              asChild
-              onClick={() => setIsShow(false)}
-              className={`rounded-lg justify-start gap-4 pl-4 flex items-center w-[90%] text-sm  transition-all ${
-                pathname === "/downloadables"
-                  ? "bg-primary text-background hover:bg-primary hover:text-background"
-                  : "bg-background"
-              }`}
-            >
-              <Link href="/downloadables">
-                <Download absoluteStrokeWidth size={22} />
-                <span>Downloadables</span>
-              </Link>
-            </Button>
-          </div>
-          <div className="flex flex-col gap-2 pt-5 mt-10 border-t h-fit">
-            <SignOutDialog>
-              <Button
-                variant="ghost"
-                size="lg"
-                className="gap-4 flex items-center w-full text-sm rounded-lg justify-start px-4"
-              >
-                <LogOut absoluteStrokeWidth size={22} />
-                <span>Signout</span>
-              </Button>
-            </SignOutDialog>
-          </div>
-        </ul>
+        <Nav isExpanded={true} />
       </div>
-    </nav>
+    </div>
   );
 }
