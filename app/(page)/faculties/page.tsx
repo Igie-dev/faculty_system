5e3cbb6233b9a7bcf98716d4165c6c9057798825
Igie Baldesanso <baldesansoigieboysanchez@gmail.com>
2024-05-16 22:18:12 +0800

    start on faculties page

--- app/(page)/faculties/page.tsx
+++ app/(page)/faculties/page.tsx
@@ -1,5 +1,19 @@
 import React from "react";
 
-export default function page() {
+export default async function page() {
+  const faculties = await getFaculties();
+  console.log(faculties);
   return <div>Faculties</div>;
+}
+
+async function getFaculties() {
+  const res = await fetch(`${process.env.URL}/api/faculty`, {
+    method: "GET",
+    cache: "no-store",
+    // next: { revalidate: 3600 },
+  });
+  if (!res?.ok) {
+    return [];
+  }
+  return await res.json();
 }
