e260be62d315a0f0224a1fa522dc97f883320479
Igie Baldesanso <baldesansoigieboysanchez@gmail.com>
2024-05-17 01:29:06 +0800

    table

---
+++ components/DataTable.tsx
@@ -0,0 +1,73 @@
+import React from "react";
+import { ColumnDef, Table, flexRender } from "@tanstack/react-table";
+import {
+  Table as T,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
+
+type Props<TData, TValue> = {
+  table: Table<TData>;
+  columns: ColumnDef<TData, TValue>[];
+};
+
+export default function DataTable<TData, TValue>({
+  columns,
+  table,
+}: Props<TData, TValue>) {
+  return (
+    <T className="min-w-[70rem] bg-background my-1 text-sm">
+      <TableHeader>
+        {table?.getHeaderGroups().map((headerGroup) => (
+          <TableRow key={headerGroup.id} className="!h-9">
+            {headerGroup.headers.map((header) => {
+              return (
+                <TableHead key={header.id} className="h-full">
+                  {header.isPlaceholder
+                    ? null
+                    : flexRender(
+                        header.column.columnDef.header,
+                        header.getContext()
+                        // eslint-disable-next-line no-mixed-spaces-and-tabs
+                      )}
+                </TableHead>
+              );
+            })}
+          </TableRow>
+        ))}
+      </TableHeader>
+      <TableBody>
+        {table?.getRowModel().rows?.length ? (
+          table?.getRowModel().rows.map((row) => (
+            <TableRow
+              key={row.id}
+              data-state={row.getIsSelected() && "selected"}
+            >
+              {row.getVisibleCells().map((cell) => (
+                <TableCell
+                  key={cell.id}
+                  className={`pl-5 border !py-2 ${
+                    row.index % 2 === 0
+                      ? "bg-primary-foreground"
+                      : "bg-background"
+                  }`}
+                >
+                  {flexRender(cell.column.columnDef.cell, cell.getContext())}
+                </TableCell>
+              ))}
+            </TableRow>
+          ))
+        ) : (
+          <TableRow>
+            <TableCell colSpan={columns?.length} className="h-24 text-center">
+              No results.
+            </TableCell>
+          </TableRow>
+        )}
+      </TableBody>
+    </T>
+  );
+}
