20b15e3072e47829cc53d20d27b56a307494ec9c
Igie Baldesanso <baldesansoigieboysanchez@gmail.com>
2024-05-17 01:28:10 +0800

    render table

--- app/(page)/faculties/page.tsx
+++ app/(page)/faculties/page.tsx
@@ -1,19 +1,13 @@
-import React from "react";
+import React, { Suspense } from "react";
+import FacultiesTable from "./components/table/FacultiesTable";
+import TableLoader from "./components/table/TableLoader";
 
 export default async function page() {
-  const faculties = await getFaculties();
-  console.log(faculties);
-  return <div>Faculties</div>;
-}
-
-async function getFaculties() {
-  const res = await fetch(`${process.env.URL}/api/faculty`, {
-    method: "GET",
-    cache: "no-store",
-    // next: { revalidate: 3600 },
-  });
-  if (!res?.ok) {
-    return [];
-  }
-  return await res.json();
+  return (
+    <div className="w-full h-full">
+      <Suspense fallback={<TableLoader />}>
+        <FacultiesTable />
+      </Suspense>
+    </div>
+  );
 }
