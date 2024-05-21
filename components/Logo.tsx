79094126d28cdc9f1a41404d6df310b8a9979977
Igie Baldesanso <baldesansoigieboysanchez@gmail.com>
2024-05-22 02:22:10 +0800

    style (logo): Update

--- components/Logo.tsx
+++ components/Logo.tsx
@@ -1,10 +1,7 @@
 import Link from "next/link";
 export default function Logo() {
   return (
-    <Link
-      href="/"
-      className="px-4 py-1  font-semibold text-lg border rounded-md bg-primary-foreground"
-    >
+    <Link href="/" className="px-4 py-1 font-semibold text-lg ">
       Faculty
     </Link>
   );
