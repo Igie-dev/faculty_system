da923de156b16db836e50a4cdbb2e17e0a361e97
Igie Baldesanso <baldesansoigieboysanchez@gmail.com>
2024-05-28 01:01:30 +0800

    update action

--- actions/departments.ts
+++ actions/departments.ts
@@ -1,16 +1,30 @@
 "use server";
+import { ERole } from "@/@types/enums";
+import { getCurrentUser } from "@/lib/auth";
 import prisma from "@/utils/prisma";
 
-export const getDepartments = async (): Promise<TDepartmentData[]> => {
+export const getDepartments = async (): Promise<{
+  data?: TDepartmentData[];
+  error?: string;
+}> => {
   try {
+    const { role: userRole } = await getCurrentUser();
+
+    if (userRole !== ERole.IS_ADMIN) {
+      return {
+        error: "Unauthorized user!",
+      };
+    }
     const departments = await prisma.department.findMany();
 
     if (departments?.length <= 0) {
-      return [];
+      return { data: [] };
     }
 
-    return departments;
+    return { data: departments };
   } catch (error) {
-    return [];
+    return {
+      error: "Something went wrong!",
+    };
   }
 };
