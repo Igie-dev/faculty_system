8ae83610ac9d5828baef8879520cf7736824d2ab
Igie Baldesanso <baldesansoigieboysanchez@gmail.com>
2024-05-30 18:54:59 +0800

    style (error page): Add error page

--- app/(auth)/autherror/page.tsx
+++ app/(auth)/autherror/page.tsx
@@ -1,11 +1,35 @@
+import { Button } from "@/components/ui/button";
 import React from "react";
+import errorPng from "@/public/error.png";
+import Image from "next/image";
+import Link from "next/link";
 type Props = {
   searchParams: {
     error?: string;
   };
 };
 export default function page({ searchParams }: Props) {
-  //TODO
-  //Handle error here
-  return <div>Auth error : {searchParams.error}</div>;
+  return (
+    <section className="w-full h-full flex items-center justify-center bg-secondary">
+      <div className="flex flex-col w-full h-fit px-4 py-8 rounded-lg bg-background md:max-w-[40rem] border items-center space-y-8">
+        <Image
+          src={errorPng}
+          alt="error image"
+          width={100}
+          height={100}
+          className="opacity-80"
+        />
+        <div className="flex flex-col space-y-2 items-center">
+          <h1 className="text-lg font-semibold text-destructive">
+            Login Failed!
+          </h1>
+          <p>{searchParams.error}</p>
+        </div>
+
+        <Button type="button" asChild className="min-w-[20rem]">
+          <Link href="/signin">Try again</Link>
+        </Button>
+      </div>
+    </section>
+  );
 }
--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,5 +1,26 @@
+"use client";
 import React from "react";
-
-export default function notfound() {
-  return <div>404 - notfound</div>;
+import { Button } from "@/components/ui/button";
+import { useRouter } from "next/navigation";
+export default function Notfound() {
+  const router = useRouter();
+  return (
+    <section className="w-screen h-screen flex items-center justify-center ">
+      <div className="flex flex-col w-full h-fit px-4 py-8  md:max-w-[40rem]  items-center space-y-10">
+        <div className="flex flex-col space-y-4 items-center">
+          <span className="font-extrabold text-destructive text-8xl lg:text-9xl">
+            404
+          </span>
+          <span className="text-sm font-semibold">This page Not found!</span>
+        </div>
+        <Button
+          type="button"
+          onClick={() => router.back()}
+          className="min-w-[10rem]"
+        >
+          Back
+        </Button>
+      </div>
+    </section>
+  );
 }
