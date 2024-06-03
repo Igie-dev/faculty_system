98ee2c83e19f4405f8d62e921803683f0429163d
Igie Baldesanso <baldesansoigieboysanchez@gmail.com>
2024-06-04 00:01:22 +0800

    feat (react query): Make single query client

--- app/service/queryClient.ts
+++ app/service/queryClient.ts
@@ -2,11 +2,11 @@ import {
   QueryClient,
   defaultShouldDehydrateQuery,
 } from "@tanstack/react-query";
-export default function makeQueryClient() {
+export function makeQueryClient() {
   return new QueryClient({
     defaultOptions: {
       queries: {
-        staleTime: 60 * 1000,
+        staleTime: 1000,
       },
       dehydrate: {
         // per default, only successful Queries are included,
@@ -17,4 +17,20 @@ export default function makeQueryClient() {
       },
     },
   });
+}
+
+let browserQueryClient: QueryClient | undefined = undefined;
+
+export function getQueryClient() {
+  if (typeof window === "undefined") {
+    // Server: always make a new query client
+    return makeQueryClient();
+  } else {
+    // Browser: make a new query client if we don't already have one
+    // This is very important, so we don't re-make a new client if React
+    // suspends during the initial render. This may not be needed if we
+    // have a suspense boundary BELOW the creation of the query client
+    if (!browserQueryClient) browserQueryClient = makeQueryClient();
+    return browserQueryClient;
+  }
 }
--- components/QueryProvider.tsx
+++ components/QueryProvider.tsx
@@ -1,9 +1,9 @@
 "use client";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { QueryClientProvider } from "@tanstack/react-query";
 import { ReactNode } from "react";
-
-const queryClient = new QueryClient();
+import { getQueryClient } from "@/app/service/queryClient";
 export default function QueryProvider({ children }: { children: ReactNode }) {
+  const queryClient = getQueryClient();
   return (
     <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
   );
