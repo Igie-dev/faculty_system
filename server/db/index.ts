b87b5196cc3d625337b52f6f605447438f32ad4a
Igie Baldesanso <baldesansoigieboysanchez@gmail.com>
2024-06-05 20:38:54 +0800

    feat (db connection): Make db connection as singleton

--- server/db/index.ts
+++ server/db/index.ts
@@ -1,12 +1,25 @@
 import { drizzle } from "drizzle-orm/node-postgres";
 import * as schema from "./schema";
-import { Client } from "pg";
+import { Pool } from "pg";
 
-export const client = new Client({
-  connectionString: process.env.DATABASE_URL as string,
-});
+function singleton<Value>(name: string, value: () => Value): Value {
+  const globalAny: any = global;
+  globalAny.__singletons = globalAny.__singletons || {};
 
-client.connect();
-export const db = drizzle(client, { schema, logger: true });
+  if (!globalAny.__singletons[name]) {
+    globalAny.__singletons[name] = value();
+  }
+
+  return globalAny.__singletons[name];
+}
+
+function createDatabaseConnection() {
+  const poolConnection = new Pool({
+    connectionString: process.env.DATABASE_URL as string,
+  });
+  return drizzle(poolConnection, { schema, logger: true });
+}
+
+export const db = singleton("db", createDatabaseConnection);
 export type db = typeof db;
 export default db;
