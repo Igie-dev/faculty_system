036c3e70214ebcdcf818c3301bfa4e81760e5b66
Igie Baldesanso <baldesansoigieboysanchez@gmail.com>
2024-06-06 22:14:10 +0800

    feat (semester page): Creare semester list, create and delete forms

---
+++ app/(page)/semesters/components/deleteSemester/DeleteSemester.tsx
@@ -0,0 +1,69 @@
+import React from 'react'
+import {
+    AlertDialog,
+    AlertDialogAction,
+    AlertDialogCancel,
+    AlertDialogContent,
+    AlertDialogDescription,
+    AlertDialogFooter,
+    AlertDialogHeader,
+    AlertDialogTitle,
+    AlertDialogTrigger,
+  } from "@/components/ui/alert-dialog";
+  import { Button } from "@/components/ui/button";
+import { Trash } from "lucide-react";
+import {  deleteSemesterById } from "@/server/actions";
+import { useToast } from "@/components/ui/use-toast";
+type Props = {
+    id:number
+    semester:string,
+}
+export default function DeleteSemester({id,semester}:Props) {
+  const { toast } = useToast();
+  const handleDelete = async () => {
+    const res = await deleteSemesterById(id);
+    if (res?.message || res?.error) {
+      toast({
+        variant: res.error ? "destructive" : "default",
+        title: res.message
+          ? "Delete semester success!"
+          : res.error
+          ? "Delete semester failed!"
+          : "",
+        description: res.message ?? res.error ?? "",
+      });
+    }
+  };
+return (
+  <AlertDialog>
+  <AlertDialogTrigger asChild>
+    <Button
+      size="icon"
+      variant="ghost"
+      className="text-muted-foreground hover:text-destructive"
+    >
+      <Trash size={16} />
+    </Button>
+  </AlertDialogTrigger>
+  <AlertDialogContent>
+    <AlertDialogHeader>
+      <AlertDialogTitle>Are you absolutely sure?</AlertDialogTitle>
+      <AlertDialogDescription>
+        This action cannot be undone. It will permanently delete the
+        semester and remove all associated data from our servers.
+      </AlertDialogDescription>
+    </AlertDialogHeader>
+    <div className="flex flex-col gap-2">
+      <span className="text-sm text-muted-foreground">Details </span>
+      <div className="flex flex-col items-start mt-2 gap-2 text-sm">
+        <span className="font-semibold text-lg">{semester + " "} Semester</span>
+      </div>
+    </div>
+    <AlertDialogFooter>
+      <AlertDialogCancel>Cancel</AlertDialogCancel>
+      <AlertDialogAction onClick={handleDelete}>Continue</AlertDialogAction>
+    </AlertDialogFooter>
+  </AlertDialogContent>
+</AlertDialog>
+)
+}
