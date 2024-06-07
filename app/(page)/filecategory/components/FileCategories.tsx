cc2af9d3cb1874c74637efe9335320e270d903bf
Igie Baldesanso <baldesansoigieboysanchez@gmail.com>
2024-06-07 22:58:11 +0800

    stye (file category page): Update layout

--- app/(page)/filecategory/components/FileCategories.tsx
+++ app/(page)/filecategory/components/FileCategories.tsx
@@ -22,10 +22,12 @@ export default function FileCategories() {
   if (isFetching) return <FileCategoryLoader />;
 
   return (
-    <section className="flex flex-col items-center w-full h-full">
-      <Header />
-      <div className="w-full flex flex-1 min-h-0 justify-center overflow-y-auto">
-        <FileCategoryList fileCategories={fileCategories} />
+    <section className="flex flex-col items-center w-full h-full md:py-2">
+      <div className="w-full flex flex-1 flex-col min-h-0  md:w-[98%] rounded-lg border bg-background">
+        <Header />
+        <div className="w-full flex flex-1 min-h-0 justify-center overflow-y-auto bg-secondary">
+          <FileCategoryList fileCategories={fileCategories} />
+        </div>
       </div>
     </section>
   );
