ac4f5b101303aa24019290efb9914f3c0b55c4f0
igie <baldesansoigieboysanchez@gmail.com>
2024-06-19 01:28:48 +0800

    Add announcement router

--- server/api/root.ts
+++ server/api/root.ts
@@ -4,12 +4,14 @@ import { schoolYearRouter } from "./routers/schoolyear";
 import { fileCategoryRouter } from "./routers/filecategory";
 import { departmentRouter } from "./routers/department";
 import { facultyRouter } from "./routers/faculty";
+import { announcementRouter } from "./routers/announcement";
 export const appRouter = createTRPCRouter({
   semester: semesterRouter,
   schoolYear: schoolYearRouter,
   fileCategory: fileCategoryRouter,
   department: departmentRouter,
   faculty: facultyRouter,
+  announcement: announcementRouter
 });
 
 // export type definition of API
