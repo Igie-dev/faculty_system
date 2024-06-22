b8b48cd9bd0ded3573a08fbf578abdba6338c07d
igie <baldesansoigieboysanchez@gmail.com>
2024-06-22 22:12:31 +0800

    add faculty details in announcement data

--- server/api/routers/announcement.ts
+++ server/api/routers/announcement.ts
@@ -6,7 +6,18 @@ import { createTRPCRouter, handleError, privateProcedure, publicProcedure } from
 export const announcementRouter = createTRPCRouter({
     getAll: publicProcedure.query(async () => {
         try {
-            const announcements = await db.query.announcement.findMany();
+            const announcements = await db.query.announcement.findMany({
+                with: {
+                    faculty: {
+                        columns: {
+                            faculty_id: true,
+                            name: true,
+                            role: true,
+                            image: true
+                        }
+                    }
+                }
+            });
             return {
                 success: true,
                 data: announcements
