073da65eb5307f554d064b42b516b4d6047f1a41
igie <baldesansoigieboysanchez@gmail.com>
2024-06-27 00:19:55 +0800

    get announcement

--- server/api/routers/announcement.ts
+++ server/api/routers/announcement.ts
@@ -1,7 +1,9 @@
 import { announcement, departmentAnnouncement } from "@/server/db/schema"
 import { db } from "@/server/db"
-import { createTRPCRouter, handleError, privateProcedure, publicProcedure } from "../trpc";
-
+import { createTRPCRouter, handleError, privateProcedure, publicProcedure } from '../trpc';
+import { z } from "zod";
+import { eq, sql } from "drizzle-orm";
+import { TRPCError } from "@trpc/server";
 
 export const announcementRouter = createTRPCRouter({
     getAll: publicProcedure.query(async () => {
@@ -21,6 +23,38 @@ export const announcementRouter = createTRPCRouter({
             return {
                 success: true,
                 data: announcements
+            }
+        } catch (error) {
+            handleError(error)
+        }
+    }),
+    getBytAnnouncementId: publicProcedure.input(z.string()).query(async ({ input }) => {
+        try {
+            const annnouncementId = input;
+            const foundAnnouncement = await db.query.announcement.findFirst({
+                where: () => sql`${announcement.announcement_id} = ${annnouncementId}`,
+                with: {
+                    faculty: {
+                        columns: {
+                            faculty_id: true,
+                            name: true,
+                            role: true,
+                            image: true
+                        }
+                    }
+                }
+            });
+
+            if (!foundAnnouncement?.id) {
+                throw new TRPCError({
+                    code: "NOT_FOUND",
+                    message: "Announcement not found!"
+                })
+            }
+
+            return {
+                success: true,
+                data: foundAnnouncement
             }
         } catch (error) {
             handleError(error)
