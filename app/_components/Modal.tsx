277845155df80918d3d909d8df5919a23d099ccd
igie <baldesansoigieboysanchez@gmail.com>
2024-06-28 01:02:13 +0800

    background change

--- app/_components/Modal.tsx
+++ app/_components/Modal.tsx
@@ -4,7 +4,7 @@ import { Dialog, DialogOverlay } from "./ui/dialog";
 export default function Modal({ children }: { children: React.ReactNode }) {
   return (
     <Dialog open>
-      <DialogOverlay className="overflow-y-auto bg-secondary/50 flex justify-center">
+      <DialogOverlay className="overflow-y-auto bg-secondary/80 flex justify-center">
         {children}
       </DialogOverlay>
     </Dialog>
